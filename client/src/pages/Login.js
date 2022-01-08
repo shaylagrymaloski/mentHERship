33965f46f7fa9861def44e76cae0b7e04a2738d1
Kimiya <kimiyamousavi@gmail.com>
2022-01-08 13:26:04 -0800

    Updated login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,20 +1,56 @@
 import "../App.css";
-import React from "react";
-import Footer from '../components/Footer';
-
-
+import React, {useState, useRef} from "react";
 
 const Login = () => {
-    return (
-      <div>
-      <p><header>HOME</header>
-      <div> hello</div> </p>
+  const [email, setEmail] = useState([])
+  const [password, setPassword] = useState([])
+  const emailRef = useRef()
+  const passwordRef = useRef()
 
-    <Footer/>
-    </div>  
+  const handleSubmit = event => {
+    event.preventDefault();
+    alert('You have submitted the form')
+    // should make it go to chat page after log in
+    // if pass or user is not authorized, print error message
+  }
 
-  
-    );
-  };
+  const handleEmailChange = event => {
+    const email = emailRef.current.value
+    if (email === '') return
+    //console.log(email)
+
+    setEmail(emailRef.current.value)
+  }
+
+  const handlePasswordChange = event => {
+    const pass = passwordRef.current.value
+    if (pass === '') return
+   // console.log(pass)
+
+    setPassword(passwordRef.current.value)
+  }
+
+
+  return(
+    <div>
+      <h1>LOGIN</h1>
+      <form>
+        <label>
+          <span>Email</span>
+          <input name="email" ref={emailRef} onChange={handleEmailChange}/>
+        </label>
+        <br></br>
+        <label>
+          <span>Password</span>
+          <input name="password" ref={passwordRef} onChange={handlePasswordChange}/>
+        </label>
+        <div>
+          <button type="submit" onClick={handleSubmit}>Submit</button>
+        </div>
+      </form>
+    </div>
+    
+  )
+};
   
   export default Login;
