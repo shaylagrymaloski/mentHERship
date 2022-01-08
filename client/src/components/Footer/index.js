694386a4f6a09846b98c6f4eab67b64c1d78976d
Shayla Grymaloski <shaylagrymaloski@Shaylas-MacBook-Pro-2.local>
2022-01-08 08:54:09 -0800

    added donate now logo

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import donate from '../../logo/DonateNow.png';
 
 import "./Footer.css";
 
@@ -10,8 +11,11 @@ const Footer = () => {
       <div class="container">
         <div class="row">
           <div class="col-sm-12 col-md-6">
-            <h6>Donate</h6>
-            <p class="text-justify"> Donations help us keep are services open!!</p>
+            <h6><Link to="/donations"><img src={donate}  alt="logo" /> 
+</Link></h6>
+            <p class="text-justify"> We rely soley on donations to fund our mentorship program.
+                    All donations go to improving our program by adding 
+                    more learning opportunities for both our mentors and mentees!</p>
           </div>
 
           
