ce2a019e1f6426cfa4158b96d23c0b62664f2a3d
Shayla Grymaloski <shaylagrymaloski@Shaylas-MacBook-Pro-2.local>
2022-01-08 09:49:56 -0800

    added to the start page

--- client/src/pages/StartPage.js
+++ client/src/pages/StartPage.js
@@ -1,12 +1,21 @@
 import "../App.css";
 import React from "react";
 import logo from '../logo-headtags.png';
+import register from '../logo/Register.png';
+import login from '../logo/LogIn.png';
+import { Link } from "react-router-dom";
+
+
+
 
 const StartPage = () => {
     return (
       <div>
             <img src={logo} className="App-logo" alt="logo" /> 
-
+            <div class="login"> 
+              <div> <Link to="/register"><img src={register} /> </Link></div>
+              <div >Already Registered? <Link to="/login"><img src={login}/> </Link> </div>
+            </div>
       </div>
   
     );
