eb4a714df30a6ddc4ff0cb3e7ca1b6245cba7b98
Shayla Grymaloski <shaylagrymaloski@Shaylas-MacBook-Pro-2.local>
2022-01-08 08:41:04 -0800

    added footer

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+   
+    <footer class="site-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12 col-md-6">
+            <h6>Donate</h6>
+            <p class="text-justify"> Donations help us keep are services open!!</p>
+          </div>
+
+          
+        </div>
+       
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-sm-6 col-xs-12">
+            <p class="copyright-text">
+        
+            </p>
+          </div>
+
+        </div>
+      </div>
+</footer>
+  );
+};
+
+export default Footer;
